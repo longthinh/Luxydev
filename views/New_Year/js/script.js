a7fa6e30364c723ebd4ee43b790ecf44469d2206
Luxydev <longthinhx@icloud.com>
2026-01-07 21:05:51 +0700

    Update

--- views/New_Year/js/script.js
+++ views/New_Year/js/script.js
@@ -2306,7 +2306,7 @@ if (IS_HEADER) {
   init();
 } else {
   // Allow status to render, then preload assets and start app.
-  // setLoadingStatus("Lighting Fuses");
+  setLoadingStatus("LUXYDEV");
   setTimeout(() => {
     soundManager.preload().then(init, (reason) => {
       // Codepen preview doesn't like to load the audio, so just init to fix the preview for now.
